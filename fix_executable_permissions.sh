2fb1c1fb968879fc43f4d09cb0079b5bed963139
Unknown <s0546867@htw-berlin.de>
2018-11-29 15:04:20 +0100

    unknown changes

--- fix_executable_permissions.sh
+++ fix_executable_permissions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 chmod +x camera_pseudo/src/CameraPseudo.py
-chmod +x exercise_1/src/MySubscriber.py
+chmod +x excercise_1/src/MySubscriber.py
 chmod +x prediction/src/Prediction.py
