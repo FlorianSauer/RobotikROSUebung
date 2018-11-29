c585a67c2c3873c4d4c57dbd937f0a7b5fdf2843
Florian Sauer <FlorianSauer@users.noreply.github.com>
2018-11-29 10:22:02 +0100

    Update MySubscriber.py

--- excercise_1/src/MySubscriber.py
+++ excercise_1/src/MySubscriber.py
@@ -2,12 +2,16 @@
 
 import traceback
 
+import keras
 import numpy
 import rospy
 from genpy import Message
-from keras import Model
+from keras import Model, Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from sensor_msgs.msg import CompressedImage
 from typing import TypeVar, Type, Generic, Any
+from cv_bridge import CvBridge
+
 
 from Libs.PythonLibs.Callback import Callback
 
@@ -56,6 +60,7 @@ class PredictionCISubscriber(CompressedImageSubscriber):
 
         self.model = model
         self.prediction_callback = prediction_callback
+        self.cv_bridge = CvBridge()
 
     def handle(self, message):
         # type: (CompressedImage) -> None
@@ -65,9 +70,10 @@ class PredictionCISubscriber(CompressedImageSubscriber):
         if self.prediction_callback.callable(prediction):
             self.prediction_callback.call(prediction)
 
+    # noinspection PyUnresolvedReferences
     def unpackMessage(self, message):
         # type: (CompressedImage) -> numpy.array
-        raise NotImplementedError
+        return self.cv_bridge.compressed_imgmsg_to_cv2(message)
 
 
 class RosSubscriberApp(object):
@@ -84,6 +90,40 @@ class RosSubscriberApp(object):
         while not rospy.is_shutdown():
             rate.sleep()
         pass
+
+    def loadMakeModel(self, path):
+        num_classes = 10
+        # input image dimensions
+        img_rows, img_cols = 28, 28
+
+        if keras.backend.image_data_format() == 'channels_first':
+            input_shape = (1, img_rows, img_cols)
+        else:
+            input_shape = (img_rows, img_cols, 1)
+
+        model = Sequential()
+        model.add(Conv2D(filters=32,
+                         kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=input_shape))
+        model.add(Conv2D(filters=64,
+                         kernel_size=(3, 3),
+                         activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(rate=0.25))
+        model.add(Flatten())
+        model.add(Dense(units=128,
+                        activation='relu'))
+        model.add(Dropout(rate=0.5))
+        model.add(Dense(units=num_classes,
+                        activation='softmax'))
+
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adadelta(),
+                      metrics=['accuracy'])
+
+        model.load_weights(filepath=path)
+        return model
 
 
 if __name__ == "__main__":
