7f21936176650c16e8aff1a1fc027835e7c0072e
Florian Sauer <FlorianSauer@users.noreply.github.com>
2018-12-04 12:50:32 +0100

    added some tests for image reshaping

--- excercise_1/src/test_rosSubscriberApp.py
+++ excercise_1/src/test_rosSubscriberApp.py
@@ -1,33 +1,66 @@
 # coding=utf-8
 import os
 from unittest import TestCase
-from MySubscriber import RosSubscriberApp
-from keras.datasets import mnist
+
 import keras
+import numpy
 import numpy as np
 from keras import backend as k
-from keras import callbacks
 from keras.datasets import mnist
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Dense, Dropout, Flatten
-from keras.models import Sequential
+
+from MySubscriber import RosSubscriberApp
 
 
-
-
-
-
-#teste die loadMakeModel darauf, ob sie aus eine Stringeingabe für einen Pfad eine Prediction abliefern kann
+# teste die loadMakeModel darauf, ob sie aus eine Stringeingabe für einen Pfad eine Prediction abliefern kann
 class TestRosSubscriberApp(TestCase):
     def getPath(self):
         return os.path.abspath(os.path.join(os.path.dirname(__file__), 'testResources/weights-best.hdf5'))
 
+    def getRandomOrigMnistImage(self, seed=None):
+        (_, _), (x_test, y_test) = mnist.load_data()
+        if seed:
+            rand_int = seed
+        else:
+            rand_int = numpy.random.randint(0, len(y_test), dtype='int')
+        return x_test[rand_int]
 
+    def getRandomReformedMnistImage(self, seed=None):
+        # the data, split between train and test sets
+        img_rows, img_cols = 28, 28
 
+        (_, _), (x_test, y_test) = mnist.load_data()
+        if seed:
+            rand_int = seed
+        else:
+            rand_int = numpy.random.randint(0, len(y_test), dtype='int')
 
+        if k.image_data_format() == 'channels_first':
+            x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+            input_shape = (1, img_rows, img_cols)
+        else:
+            x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+            input_shape = (img_rows, img_cols, 1)
 
+        x_test = x_test.astype('float32')
+        x_test /= 255.
+        return x_test[rand_int]
 
-#teste ob das Model erfolgereiche Vorhersagen trifft
+    def test_reshapeImage(self):
+        seed = 6
+        orig_mnist = self.getRandomOrigMnistImage(seed)
+        reshaped_mnist = self.getRandomReformedMnistImage(seed)
+        orig_mnist = orig_mnist.astype('float32')
+        orig_mnist /= 255.
+        orig_mnist = orig_mnist.reshape(28, 28, 1)
+        print repr(orig_mnist)
+        print '#'*25
+        print repr(reshaped_mnist)
+        print reshaped_mnist == orig_mnist
+        print reshaped_mnist.shape
+        print orig_mnist.shape
+        # self.assertEquals(orig_mnist, reshaped_mnist)
+
+    # teste ob das Model erfolgereiche Vorhersagen trifft
     def test_loadMakeModel(self):
 
         # params
@@ -66,9 +99,8 @@ class TestRosSubscriberApp(TestCase):
 
         # data to choose
 
-        model= RosSubscriberApp.loadMakeModel(self.getPath())
+        model = RosSubscriberApp.loadMakeModel(self.getPath())
         for index in xrange(100):
-
             # expand dimension for batch
             input_data = np.expand_dims(x_test[index], axis=0)  # tensorflow
             input_label = y_test[index]
