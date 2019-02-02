c57bca7180c7542d58c212a55220cbe9562015ea
Florian Sauer <FlorianSauer@users.noreply.github.com>
2019-02-03 00:21:15 +0100

    fixing paths

--- make_cython.sh
+++ make_cython.sh
@@ -2,6 +2,5 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cd $SCRIPTPATH/camera_pseudo/src/ && python setup.py build_ext --inplace; cd $SCRIPTPATH
-cd $SCRIPTPATH/exercise_1/src/ && python setup.py build_ext --inplace; cd $SCRIPTPATH
 cd $SCRIPTPATH/prediction/src/ && python setup.py build_ext --inplace; cd $SCRIPTPATH
+cd $SCRIPTPATH/sound/src/ && python setup.py build_ext --inplace; cd $SCRIPTPATH
